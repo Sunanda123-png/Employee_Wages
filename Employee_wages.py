835a1f498ee753cd3e0bde758a02d3303bde617d
Sunanda123-png <sunanda02@gmail.com>
2021-12-09 13:30:06 +0530

    ADD:calculate the daily wages where time and amount is given

--- Employee_wages.py
+++ Employee_wages.py
@@ -7,11 +7,15 @@ logging.basicConfig(filename="Employee_wages.log", filemode="w")
 
 
 class Employeewages:
+
     def employee_wages(self):
         IS_FULL_TIME = 1
+        WAGES_PER_Hour = 20
+        FULL_DAY_HOUR = 8
         empCheck = random.randint(0, 1)
         if empCheck == IS_FULL_TIME:
             print("Employee is present")
+            print("Daily wages of the employee is:- "+str(WAGES_PER_Hour*FULL_DAY_HOUR))
         else:
             print("Employee is not present")
 
