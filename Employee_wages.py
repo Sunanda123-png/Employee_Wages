1e644198ca3de0ecf2a4bf9eafeeefcb64c32ce4
Sunanda123-png <sunanda02@gmail.com>
2021-12-09 13:57:19 +0530

    ADD:Adding the part time employee

--- Employee_wages.py
+++ Employee_wages.py
@@ -9,13 +9,18 @@ logging.basicConfig(filename="Employee_wages.log", filemode="w")
 class Employeewages:
 
     def employee_wages(self):
-        IS_FULL_TIME = 1
+        PART_TIME=1
+        IS_FULL_TIME = 2
         WAGES_PER_Hour = 20
         FULL_DAY_HOUR = 8
-        empCheck = random.randint(0, 1)
+        PART_DAY_HOUR = 4
+        empCheck = random.randint(0, 3)
         if empCheck == IS_FULL_TIME:
             print("Employee is present")
             print("Daily wages of the employee is:- "+str(WAGES_PER_Hour*FULL_DAY_HOUR))
+        elif empCheck == PART_TIME:
+            print("Employee is part time present")
+            print("Daily wages of the employee is:- " + str(WAGES_PER_Hour * PART_DAY_HOUR))
         else:
             print("Employee is not present")
 
